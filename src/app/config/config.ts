fcc68e48476173271dfd56be94670544c9e9b0ec
Nemanja Milošević <nemanjamilosevic117@gmail.com>
2024-02-15 13:48:13 +0100

    Update config.ts

--- src/app/config/config.ts
+++ src/app/config/config.ts
@@ -1,8 +1,8 @@
 export const config = {
-    API_URL: 'https://localhost:9001/api/v1',
+    API_URL: '',
     JWT_KEY: 'JWT-KEY',
     USER_KEY: 'USER-KEY',
-    CLIENT_ID: '0bbf0469ef921cf211b8',
+    CLIENT_ID: '',
     CALLBACK_URL: 'https://localhost:4200/callback',
     GITHUB_OAUTH2: function () {
         return `https://github.com/login/oauth/authorize?client_id=${this.CLIENT_ID}&redirect_uri=${this.CALLBACK_URL}`
